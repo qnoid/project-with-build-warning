4ef03148f8acdfd0617de67608ac1371f8517ef0
Markos Charatzas <markos@qnoid.com>
2019-05-17 18:03:40 +0300

    Initial Commit

---
+++ project-with-build-warning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project-with-build-warning
+//
+//  Created by Markos Charatzas on 17/05/2019.
+//  Copyright © 2019 Markos Charatzas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project-with-build-warningTests/project_with_build_warningTests.swift
@@ -0,0 +1,34 @@
+//
+//  project_with_build_warningTests.swift
+//  project-with-build-warningTests
+//
+//  Created by Markos Charatzas on 17/05/2019.
+//  Copyright © 2019 Markos Charatzas. All rights reserved.
+//
+
+import XCTest
+@testable import project_with_build_warning
+
+class project_with_build_warningTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
